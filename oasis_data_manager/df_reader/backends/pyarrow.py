feb1d8caddb4e5c371af8d327a579bf30a742e4b
sstruzik <stephane.struzik@gmail.com>
2025-05-15 09:02:36 +0100

    pep8

--- oasis_data_manager/df_reader/backends/pyarrow.py
+++ oasis_data_manager/df_reader/backends/pyarrow.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 from .base import OasisReader
 
+
 class OasisPyarrowReader(OasisReader):
 
     def read_parquet(self, *args, **kwargs):
@@ -48,7 +49,6 @@ class OasisPyarrowReader(OasisReader):
 
             return new_filter
 
-
         if 'filters' in kwargs:
             ds_filter = pyarrow_dataset_filter(kwargs['filters'])
         else:
@@ -65,7 +65,7 @@ class OasisPyarrowReader(OasisReader):
                     self.filename_or_buffer, encode_params=False
                 )
 
-                uri = uri.replace('file://', '') 
+                uri = uri.replace('file://', '')
                 dataset = ds.dataset(uri, partitioning='hive')
                 self.df = dataset.to_table(filter=ds_filter).to_pandas()
 
