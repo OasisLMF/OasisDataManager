924d71dc4e2bce91c7e3fc28b204c4c346f9952a
Sam Gamble <hexadessa@gmail.com>
2026-06-10 10:43:11 +0100

    test fix

--- oasis_data_manager/filestore/backends/base.py
+++ oasis_data_manager/filestore/backends/base.py
@@ -458,9 +458,10 @@ class BaseStorage(object):
     def open(self, path, *args, **kwargs):
         if self._is_valid_url(path):
             with tempfile.TemporaryDirectory() as d:
-                with open(
-                    self.get_from_cache(path, no_cache_target=os.path.join(d, "f"))
-                ) as f:
+                local_path = self.get_from_cache(path, no_cache_target=os.path.join(d, "f"))
+                if local_path is None:
+                    raise FileNotFoundError(f"No such file or directory: '{path}'")
+                with open(local_path) as f:
                     yield f
         else:
             with self.fs.open(path, *args, **kwargs) as f:
@@ -470,7 +471,8 @@ class BaseStorage(object):
     def with_fileno(self, path, mode="rb"):
         with tempfile.TemporaryDirectory() as d:
             target = os.path.join(d, "fileno")
-            path = self.get_from_cache(path, no_cache_target=target)
-
-            with open(path, mode) as f:
+            local_path = self.get_from_cache(path, no_cache_target=target)
+            if local_path is None:
+                raise FileNotFoundError(f"No such file or directory: '{path}'")
+            with open(local_path, mode) as f:
                 yield f
